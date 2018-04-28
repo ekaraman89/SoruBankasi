3f08d6abba820a32c8af588b4d3e416f723bfbe1
emrah karaman <ekaraman89@hotmail.com>
2018-04-29 00:24:20 +0300

    Profile page links added

--- SoruBankasi/App_Start/RouteConfig.cs
+++ SoruBankasi/App_Start/RouteConfig.cs
@@ -19,6 +19,10 @@ namespace SoruBankasi
             routes.MapRoute(name: "Logout", url: "CiksiYap", defaults: new { controller = "Account", action = "Logout" });
             routes.MapRoute(name: "ProfileDetail", url: "Profil", defaults: new { controller = "Account", action = "ProfileDetail" });
 
+            routes.MapRoute(name: "ChangeMail", url: "ChangeMail", defaults: new { controller = "Account", action = "ChangeMail" });
+            routes.MapRoute(name: "ChangePhoto", url: "ChangePhoto", defaults: new { controller = "Account", action = "ChangePhoto" });
+            routes.MapRoute(name: "ChangePassword", url: "ChangePassword", defaults: new { controller = "Account", action = "ChangePassword" });
+
         }
     }
 }
