f0c26a36783550b431c0f2bfdb9e4375c8e4ae6a
emrah karaman <ekaraman89@hotmail.com>
2018-04-29 00:34:03 +0300

    remove unnecessary usings

--- SoruBankasi/Controllers/AccountController.cs
+++ SoruBankasi/Controllers/AccountController.cs
@@ -1,7 +1,5 @@
 ﻿using SoruBankasi.Helpers;
 using SoruBankasi.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
--- SoruBankasi/Models/SoruBankasiDbContext.cs
+++ SoruBankasi/Models/SoruBankasiDbContext.cs
@@ -1,9 +1,6 @@
 namespace SoruBankasi.Models
 {
-    using System;
     using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Linq;
     using System.Collections.Generic;
 
     public partial class SoruBankasiDbContext : DbContext
