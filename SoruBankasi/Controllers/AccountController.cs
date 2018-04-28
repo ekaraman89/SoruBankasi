3b0d2b81e9ecfa35b267af134abf81331e4cb632
emrah karaman <ekaraman89@hotmail.com>
2018-04-29 00:24:50 +0300

    Profile actions added

--- SoruBankasi/Controllers/AccountController.cs
+++ SoruBankasi/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using SoruBankasi.Models;
+﻿using SoruBankasi.Helpers;
+using SoruBankasi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,8 +29,8 @@ namespace SoruBankasi.Controllers
                     if (kul != null)
                     {
                         FormsAuthentication.SetAuthCookie(kul.KullaniciAdi, true);
-                    
-                        if (!string.IsNullOrWhiteSpace(ReturnUrl))                           
+
+                        if (!string.IsNullOrWhiteSpace(ReturnUrl))
                             return Redirect(ReturnUrl);
                         return RedirectToRoute("Home");
                     }
@@ -46,15 +47,97 @@ namespace SoruBankasi.Controllers
             return RedirectToRoute("Login");
         }
 
-
+        [Authorize]
         public ActionResult ProfileDetail()
         {
             using (SoruBankasiDbContext db = new SoruBankasiDbContext())
             {
+                if (TempData["Message"] != null)
+                    ViewBag.Message = TempData["Message"].ToString();
+
                 return View(db.Kullanici.SingleOrDefault(x => x.KullaniciAdi.Equals(User.Identity.Name)));
             }
-
         }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult ChangeMail(Kullanici model)
+        {
+            ModelState.Remove("Sifre");
+            ModelState.Remove("KullaniciAdi");
+            if (ModelState.IsValid)
+            {
+                using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+                {
+                    Kullanici kullanici = db.Kullanici.SingleOrDefault(x => x.KullaniciAdi.Equals(User.Identity.Name));
+                    if (db.Kullanici.SingleOrDefault(x => !x.Mail.Equals(model.Mail) && x.KullaniciAdi != kullanici.KullaniciAdi) != null)
+                    {
+                        kullanici.Mail = model.Mail;
+                        db.Entry(kullanici).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        TempData["Message"] = $"<div class='alert alert-success'><strong>Başarılı!</strong> Bilgileriniz Başarıyla Güncellendi... </div>";
+                    }
+                    else
+                    {
+                        TempData["Message"] = $"<div class='alert alert-danger'><strong>Hata!</strong> Mail adresi kullanılıyor... </div>";
+                    }
+                }
+            }
+            return RedirectToRoute("ProfileDetail");
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult ChangePhoto(HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+                {
+                    Kullanici kullanici = db.Kullanici.SingleOrDefault(x => x.KullaniciAdi.Equals(User.Identity.Name));
+                    kullanici.Foto = FileUpload.FileName(image, FileUpload.UploadFolder.Profile);
+                    db.Entry(kullanici).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    TempData["Message"] = $"<div class='alert alert-success'><strong>Başarılı!</strong> Profil Fotoğrafınız Başarıyla Güncellendi... </div>";
+                }
+            }
+            return Redirect("Profil#tab_1_2");
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult ChangePassword(Kullanici model)
+        {
+            ModelState.Remove("KullaniciAdi");
+            ModelState.Remove("Adi");
+            ModelState.Remove("Soyadi");
+            ModelState.Remove("Mail");
+            if (ModelState.IsValid)
+            {
+                using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+                {
+                    Kullanici kullanici = db.Kullanici.SingleOrDefault(x => x.KullaniciAdi.Equals(User.Identity.Name));
+
+                    if (kullanici.Sifre.Equals(model.Sifre))
+                    {
+                        kullanici.Sifre = model.YeniSifre;
+                        db.Entry(kullanici).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        TempData["Message"] = $"<div class='alert alert-success'><strong>Başarılı!</strong> Profil Fotoğrafınız Başarıyla Güncellendi... </div>";
+                    }
+                    else
+                    {
+                        TempData["Message"] = $"<div class='alert alert-danger'><strong>Hata!</strong> MEvcut Şifreniz yanlış... </div>";
+                    }
+                }
+            }
+            else
+            {
+                TempData["Message"] = $"<div class='alert alert-danger'><strong>Hata!</strong> Şifre Bilgilerinde hata oluştu... </div>";
+            }
+            return Redirect("Profil#tab_1_3");
+        }
+
 
     }
 }
