875508c946d4257510b735daea722890351e5e61
emrah karaman <ekaraman89@hotmail.com>
2018-05-02 20:53:39 +0300

    UsersController added

---
+++ SoruBankasi/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using SoruBankasi.Infrastructure;
+using SoruBankasi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoruBankasi.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [SelectedTab("Users")]
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+            {
+                return View(db.Kullanici.ToList());
+            }
+        }
+    }
+}
