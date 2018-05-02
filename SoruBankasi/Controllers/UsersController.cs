d14172c37db65974b6c9606417ab9f8f79202910
emrah karaman <ekaraman89@hotmail.com>
2018-05-02 23:04:54 +0300

    user controller added

--- SoruBankasi/Controllers/UsersController.cs
+++ SoruBankasi/Controllers/UsersController.cs
@@ -1,9 +1,8 @@
-﻿using SoruBankasi.Infrastructure;
+﻿using Newtonsoft.Json;
+using SoruBankasi.Infrastructure;
 using SoruBankasi.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SoruBankasi.Controllers
@@ -18,6 +17,115 @@ namespace SoruBankasi.Controllers
             {
                 return View(db.Kullanici.ToList());
             }
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Kullanici model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+                {
+                    List<Kullanici> lst = db.Kullanici.ToList();
+
+                    if (lst.SingleOrDefault(x => x.Mail.Equals(model.Mail)) == null)
+                    {
+                        if (lst.SingleOrDefault(x => x.KullaniciAdi.Equals(model.KullaniciAdi)) == null)
+                        {
+                            db.Entry(model).State = System.Data.Entity.EntityState.Added;
+                            db.SaveChanges();
+                            ViewBag.Message = $"<div class='alert alert-success'><strong>Başarılı!</strong> Kullanıcı Başarıyla Eklendi... </div>";
+                            ModelState.Clear();
+                        }
+                        else
+                        {
+                            ViewBag.Message = $"<div class='alert alert-danger'><strong>Hata!</strong> Bu kullanıcı adı zaten kullanılıyor... </div>";
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Message = $"<div class='alert alert-danger'><strong>Hata!</strong> Bu mail zaten kullanılıyor... </div>";
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int ID)
+        {
+            using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+            {
+                Kullanici kullanici = db.Kullanici.SingleOrDefault(x => x.ID.Equals(ID));
+                if (kullanici != null)
+                {
+                    return View(kullanici);
+                }
+            }
+            return RedirectToRoute("Users");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Kullanici model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+                {
+                    List<Kullanici> lst = db.Kullanici.ToList();
+
+                    if (lst.SingleOrDefault(x => x.Mail.Equals(model.Mail) && x.ID != model.ID) == null)
+                    {
+                        if (lst.SingleOrDefault(x => x.KullaniciAdi.Equals(model.KullaniciAdi) && x.ID != model.ID) == null)
+                        {
+                            Kullanici kullanici = lst.SingleOrDefault(x => x.ID.Equals(model.ID));
+                            kullanici.KullaniciAdi = model.KullaniciAdi;
+                            kullanici.Adi = model.Adi;
+                            kullanici.Soyadi = model.Soyadi;
+                            kullanici.Sifre = model.Sifre;
+                            kullanici.Mail = model.Mail;
+
+                            db.SaveChanges();
+                            ViewBag.Message = $"<div class='alert alert-success'><strong>Başarılı!</strong> Kullanıcı Başarıyla Güncellendi... </div>";
+                            ModelState.Clear();
+                        }
+                        else
+                        {
+                            ViewBag.Message = $"<div class='alert alert-danger'><strong>Hata!</strong> Bu kullanıcı adı zaten kullanılıyor... </div>";
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Message = $"<div class='alert alert-danger'><strong>Hata!</strong> Bu mail zaten kullanılıyor... </div>";
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public string Delete(int ID)
+        {
+            string message = string.Empty;
+            using (SoruBankasiDbContext db = new SoruBankasiDbContext())
+            {
+                Kullanici kullanici = db.Kullanici.SingleOrDefault(x => x.ID.Equals(ID) && !x.KullaniciAdi.Equals(User.Identity.Name));
+                if (kullanici != null)
+                {
+                    db.Kullanici.Remove(kullanici);
+                    db.SaveChanges();
+                    message = JsonConvert.SerializeObject(new { durum = "OK", mesaj = "Kullanıcı Silindi" });
+                }
+                else
+                {
+                    message = JsonConvert.SerializeObject(new { durum = "No", mesaj = "Kullanıcı Silinemedi" });
+                }
+            }
+            return message;
         }
     }
 }
