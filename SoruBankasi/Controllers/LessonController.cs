d851657c04778dd6f5bb7669a5f2ffdbec2d0929
emrah karaman <ekaraman89@hotmail.com>
2018-05-02 21:40:00 +0300

    remove unnecessary usings

--- SoruBankasi/Controllers/LessonController.cs
+++ SoruBankasi/Controllers/LessonController.cs
@@ -1,10 +1,7 @@
 ﻿using Newtonsoft.Json;
 using SoruBankasi.Infrastructure;
 using SoruBankasi.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SoruBankasi.Controllers
