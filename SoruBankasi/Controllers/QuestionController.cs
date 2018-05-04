c8bbc2d261cc48d640096acaf5967fec668ceee2
emrah karaman <ekaraman89@hotmail.com>
2018-05-04 20:24:18 +0300

    Question controller added

---
+++ SoruBankasi/Controllers/QuestionController.cs
@@ -0,0 +1,14 @@
+﻿using SoruBankasi.Infrastructure;
+using System.Web.Mvc;
+
+namespace SoruBankasi.Controllers
+{
+    [SelectedTab("Topic")]
+    public class QuestionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
