713bec0a062a3f638859d55f8aec69225d5408c8
emrah karaman <ekaraman89@hotmail.com>
2018-05-06 00:45:19 +0300

    viewmodel for exam

---
+++ SoruBankasi/Models/ViewModel/SinavHazirlamaViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SoruBankasi.Models.ViewModel
+{
+    public class SinavHazirlamaViewModel
+    {
+        public int Ders { get; set; }
+        public int Donem { get; set; }
+        public int KlasikSoruAdet { get; set; }
+        public int BoslukSoruAdet { get; set; }
+        public int TestSoruAdet { get; set; }
+
+    }
+}
