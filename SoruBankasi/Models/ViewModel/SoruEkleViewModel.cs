f411eebc026557a03adae6b88da6ffd7e801647b
emrah karaman <ekaraman89@hotmail.com>
2018-05-05 03:05:39 +0300

    create view model for question add view

---
+++ SoruBankasi/Models/ViewModel/SoruEkleViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SoruBankasi.Models.ViewModel
+{
+    public class SoruEkleViewModel
+    {
+        public int KonuID { get; set; }
+        public int DonemID { get; set; }
+        public int SoruTipiID { get; set; }
+        public string Soru { get; set; }
+        public Cevap[] Cevaplar { get; set; }
+
+        public class Cevap
+        {
+            public bool Val { get; set; }
+            public string CevapIcerik { get; set; }
+        }
+    }
+}
