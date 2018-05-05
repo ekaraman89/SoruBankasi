751a1fd8a076ac3534dad1614a660e6e9daad46f
emrah karaman <ekaraman89@hotmail.com>
2018-05-06 00:43:39 +0300

    number js added

--- SoruBankasi/Content/assets/pages/scripts/table-datatables-buttons.js
+++ SoruBankasi/Content/assets/pages/scripts/table-datatables-buttons.js
@@ -4,6 +4,8 @@ var TableDatatablesButtons = function () {
 
         var table = $('#list');
 
+        var table2 = $('.list2');
+
         var oTable = table.dataTable({
 
             //"language": {
@@ -50,6 +52,31 @@ var TableDatatablesButtons = function () {
                 //        alert('Custom Button');
                 //    }
                 //}
+            ],
+
+            "order": [
+                [0, 'asc']
+            ],
+
+            "lengthMenu": [
+                [5, 10, 15, 20, -1],
+                [5, 10, 15, 20, "All"] // change per page values here
+            ],
+            // set the initial value
+            "pageLength": 10,
+
+            "dom": "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // horizobtal scrollable datatable
+
+        });
+        var oTable2 = table2.dataTable({
+
+           
+            "language": {
+                url: 'https://cdn.datatables.net/plug-ins/1.10.16/i18n/Turkish.json'
+            },
+
+            buttons: [
+                { extend: 'pdf', className: 'btn default' }
             ],
 
             "order": [
