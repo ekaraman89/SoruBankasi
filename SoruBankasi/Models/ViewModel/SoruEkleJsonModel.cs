bbc67b6b0558c7b4eb2cc1b221f707e75b346565
emrah karaman <ekaraman89@hotmail.com>
2018-05-07 18:29:07 +0300

    jsonmodel for questionadd view

---
+++ SoruBankasi/Models/ViewModel/SoruEkleJsonModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace SoruBankasi.Models.ViewModel
+{
+    public class SoruEkleJsonModel
+    {
+
+
+        public int KonuID { get; set; }
+        public string KonuAdi { get; set; }
+        public List<DonemJson> Donem = new List<DonemJson>();
+
+
+
+        public class DonemJson
+        {
+            public int DonemID { get; set; }
+            public string DonemAdi { get; set; }
+        }
+    }
+
+}
