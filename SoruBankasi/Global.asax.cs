80bbc61b881b34c2ee60c9310477447ac2da961d
Emrah KARAMAN <ekaraman89@hotmail.com>
2019-01-21 16:53:36 +0300

    get all controller list

--- SoruBankasi/Global.asax.cs
+++ SoruBankasi/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -13,6 +14,20 @@ namespace SoruBankasi
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            Assembly asm = Assembly.GetExecutingAssembly();
+
+            asm.GetTypes()
+                .Where(type => typeof(Controller).IsAssignableFrom(type)) //filter controllers
+                .SelectMany(type => type.GetMethods())
+                .Where(method => method.IsPublic && !method.IsDefined(typeof(NonActionAttribute)));
+
+            var controlleractionlist = asm.GetTypes()
+                .Where(type => typeof(System.Web.Mvc.Controller).IsAssignableFrom(type))
+                .SelectMany(type => type.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public))
+                .Where(m => !m.GetCustomAttributes(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), true).Any())
+                .Select(x => new { Controller = x.DeclaringType.Name, Action = x.Name, ReturnType = x.ReturnType.Name, Attributes = String.Join(",", x.GetCustomAttributes().Select(a => a.GetType().Name.Replace("Attribute", ""))) })
+                .OrderBy(x => x.Controller).ThenBy(x => x.Action).ToList();
         }
     }
 }
